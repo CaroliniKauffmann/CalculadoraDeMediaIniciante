baf12afb2ac9a06c7d192360e2c0f1b0a1bdb5af
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-07-26 18:06:28 -0300

    Iniciando projeto

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "me.carolini"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    testImplementation(kotlin("test"))
+    implementation(kotlin("script-runtime"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/test/kotlin/MinhaMediaKtTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+
+internal class MinhaMediaKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun main() {
+
+    }
+}
